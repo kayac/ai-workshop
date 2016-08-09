0fac27b710b7823a3f0dfba6f7625d5685322cf6
mail@takashicompany.com <sato-takashi@KYCNT1404-04.local>
2016-08-09 17:17:56 +0900

    add egg

---
+++ Assets/Scenes/GameScene/Scripts/Egg/GameEgg.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 卵 : 一定時間で孵化する
+/// </summary>
+public class GameEgg : MonoBehaviour
+{
+	/// <summary>
+	/// 産まれるまでの時間(秒)
+	/// </summary>
+	public float hatchTime = 10f;
+
+	/// <summary>
+	/// 現在経過している時間(秒)
+	/// </summary>
+	private float _currentTime;
+
+	/// <summary>
+	/// 孵化した時に呼ばれる
+	/// </summary>
+	public event System.Action<GameEgg> onHatch;
+
+	/// <summary>
+	/// 既に孵化しているか
+	/// </summary>
+	/// <returns></returns>
+	public bool alreadyHatch { get; private set; }
+
+	void Update()
+	{
+		if (!alreadyHatch)
+		{
+			_currentTime += Time.deltaTime;
+
+			if (hatchTime <= _currentTime)
+			{
+				if (onHatch != null)
+				{
+					onHatch(this);
+				}
+				alreadyHatch = true;
+			}
+		}
+	}
+}
