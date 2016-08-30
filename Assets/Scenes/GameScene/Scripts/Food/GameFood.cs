b61399c1c42123a8ede2be54654c13ca4213f4a3
takashicompany <mail@takashicompany.com>
2016-08-30 22:50:05 +0900

    food appear animation

--- Assets/Scenes/GameScene/Scripts/Food/GameFood.cs
+++ Assets/Scenes/GameScene/Scripts/Food/GameFood.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
+using DG.Tweening;
 
 public class GameFood : GameCarriedObject
 {
 	[SerializeField]
 	private SpriteRenderer _renderer;
-	
+
 	public Const.FoodType foodType { get; private set; }
+
+	void OnEnable()
+	{
+		PlayAppear();
+	}
 
 	public void SetUp(Const.FoodType foodType)
 	{
 		this.foodType = foodType;
+	}
+
+	public void PlayAppear()
+	{
+		_renderer.transform.localScale = Vector3.zero;
+		_renderer.transform.DOScale(Vector3.one, 0.5f).SetEase(Ease.OutBounce);
 	}
 
 	public override void OnCarriedStart(GameCharacter character)
